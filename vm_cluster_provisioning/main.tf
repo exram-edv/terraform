0a259111d7d8757a1004a2825e81888cecf1bead
Wolfgang <wl@exram.de>
2017-08-31 18:10:02 +0200

    vm_cluster_provisioning hinzugefügt

---
+++ vm_cluster_provisioning/main.tf
@@ -0,0 +1,42 @@
+provider "azurerm" {
+  subscription_id = "${var.provider_azurerm_subscription_id}"
+  client_id       = "${var.provider_azurerm_client_id}"
+  client_secret   = "${var.provider_azurerm_client_secret}"
+  tenant_id       = "${var.provider_azurerm_tenant_id}"
+}
+
+resource "null_resource" "cluster" {
+    count = "${length(var.cluster_node_ids)}"
+
+    triggers {
+        node_ids ="${element(var.cluster_node_ids, count.index)}"
+    }
+
+    provisioner "file" {
+        source = "${provision_script_path}/${var.provision_script_file}"
+        destination = "/home/${var.os_admin_username}/${var.provision_script_file}"
+
+        connection {
+            type        = "ssh"
+            host        = "${element(var.cluster_node_public_ips, count.index)}"
+            user        = "${var.os_admin_username}"
+            password    = "${var.os_admin_password}"
+        }
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo apt-get -y install dos2unix",
+            "sudo dos2unix /home/${var.os_admin_username}/${var.provision_script_file}",
+            "chmod +x /home/${var.os_admin_username}/${var.provision_script_file}",
+            "/home/${var.os_admin_username}/${var.provision_script_file}" "${count.index}"
+        ]
+
+        connection {
+            type        = "ssh"
+            host        = "${element(var.cluster_node_public_ips, count.index)}"
+            user        = "${var.os_admin_username}"
+            password    = "${var.os_admin_passport}"
+        }
+    }
+}
---
+++ vm_cluster_provisioning/vars.tf
@@ -0,0 +1,33 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# VARIABLE-DEFINITION FOR AZURE ACCOUNT
+# ---------------------------------------------------------------------------------------------------------------------
+variable "provider_azurerm_subscription_id" {}
+variable "provider_azurerm_client_id" {}
+variable "provider_azurerm_client_secret" {}
+variable "provider_azurerm_tenant_id" {}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# VARIABLE-DEFINITION FOR OS
+# ---------------------------------------------------------------------------------------------------------------------
+variable "os_admin_username" {}
+variable "os_admin_password" {}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# VARIABLE-DEFINITION FOR PROVISIONING
+# ---------------------------------------------------------------------------------------------------------------------
+variable "cluster_node_ids" {
+    type = "list"
+    description = "list of vm-id´s for cluster nodes"
+}
+variable "cluster_node_public_ips" {
+    type = "list"
+    description = "list of public-ip´s for cluster nodes"
+}
+variable "provision_script_path" {
+    default = ""
+    description = "Path to the additional provisioning script"
+}
+variable "provision_script_file" {
+    default = "provision.sh"
+    description = "Filename for additional provisioning script"
+}
