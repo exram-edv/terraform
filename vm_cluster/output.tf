4b2f0b9f8e1028928ae5cefca0eaf18a98cdc75e
Wolfgang <wl@exram.de>
2017-08-31 14:46:11 +0200

    output node_ids added

--- vm_cluster/output.tf
+++ vm_cluster/output.tf
@@ -2,6 +2,10 @@ output "node_names" {
     value = ["${azurerm_virtual_machine.vm.*.name}"]
 }
 
+output "node_ids" {
+    value = ["${azurerm_virtual_machine.vm.*.id}"]
+}
+
 output "node_public_ips" {
     value = ["${azurerm_public_ip.public_ip.*.ip_address}"]
 }
